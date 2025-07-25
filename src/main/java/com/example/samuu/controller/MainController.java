93514f2c23d2fa0a3669800591bfd8ed15c1d3e0
samruddhi2409-sudo <17samruddhimahajan@gmail.com>
2025-07-25 16:51:05 +0530

    Maven is added

--- src/main/java/com/example/samuu/controller/MainController.java
+++ src/main/java/com/example/samuu/controller/MainController.java
@@ -1,12 +1,14 @@
+
+
 package com.example.samuu.controller;
 
-import org.springframework.sterotypeController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-@RequestMapping("/")
-public String index() {
-	return"forward:/index.html";
-}
+    @RequestMapping("/")
+    public String index() {
+        return "index";  // make sure there's an index.html in src/main/resources/templates if you're using Thymeleaf
+    }
 }
