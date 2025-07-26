f118a551d58fb7ae4fb515367c683c50cf2166a1
samruddhi2409-sudo <17samruddhimahajan@gmail.com>
2025-07-26 12:25:19 +0530

    Docker is added

--- src/main/java/com/example/samuu/controller/MainController.java
+++ src/main/java/com/example/samuu/controller/MainController.java
@@ -1,14 +1,14 @@
 
-
-package com.example.samuu.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-public class MainController {
-    @RequestMapping("/")
-    public String index() {
-        return "index";  // make sure there's an index.html in src/main/resources/templates if you're using Thymeleaf
-    }
-}
+//
+//package com.example.samuu.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//public class MainController {
+//    @RequestMapping("/")
+//    public String index() {
+//        return "index";  // make sure there's an index.html in src/main/resources/templates if you're using Thymeleaf
+//    }
+//}
